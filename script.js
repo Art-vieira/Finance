7b8e4bb77c67fe1bbf57d47dea40c6bd82d4d172
Art-vieira <151460516+Art-vieira@users.noreply.github.com>
2025-06-06 22:49:44 -0300

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+    const botaovolta = document.querySelector(".voltar");
+    const botaosimula = document.querySelector(".next");
+
     const taxas = {
         'Caixa': 1.0487,
         'Santander': 1.0567,
@@ -6,5 +10,51 @@ document.addEventListener("DOMContentLoaded", () => {
         'Itaú': 1.0519,
         'Nubank': 1.0595,
         'Inter': 1.0602
+    };
+
+    function formatarCampo(valor) {
+        return Number(valor).toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        });
+    }
+
+    if (botaosimula) {
+        botaosimula.addEventListener('click', () => {
+            const valorinput = document.getElementById('amount').value.replace(/\D/g, '');
+            const banco = document.getElementById('banco').value;
+            const parcela = document.getElementById('parcela').value;
+            const mesesopcoes = [6, 12, 18, 24, 36, 48, 60, 72];
+
+            if (!valorinput || !banco || !parcela) {
+                alert("Preencha todos os campos!");
+                return;
+            }
+
+            const valor = parseFloat(valorinput);
+            const meses = mesesopcoes[parcela - 1];
+            const taxa = taxas[banco];
+            const valorfinal = valor * Math.pow(taxa, meses);
+            const valorparcela = valorfinal / meses;
+
+            // usar na outra pagina
+            sessionStorage.setItem('parcela', formatarCampo(valorparcela));
+            sessionStorage.setItem('valorfinal', formatarCampo(valorfinal));
+            sessionStorage.setItem('meses', `${meses} meses`);
+
+            window.location.href = "simulacao.html";
+        });
+    }
+
+    // outra pagina
+
+    if (botaovolta) {
+        document.querySelector('.parcela').textContent = sessionStorage.getItem('parcela');
+        document.querySelector('.valor_final').textContent = sessionStorage.getItem('valorfinal');
+        document.querySelector('.financia').textContent = sessionStorage.getItem('meses');
+
+        botaovolta.addEventListener('click', () => {
+            window.location.href = "index.html";
+        });
     }
 })

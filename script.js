b75f4a16d3a3c2bf9c5e2f985ff32ece386d3d29
Art-vieira <151460516+Art-vieira@users.noreply.github.com>
2025-06-06 21:53:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const taxas = {
+        'Caixa': 1.0487,
+        'Santander': 1.0567,
+        'Bradesco': 1.0532,
+        'Itaú': 1.0519,
+        'Nubank': 1.0595,
+        'Inter': 1.0602
+    }
+})
